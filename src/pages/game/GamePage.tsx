ef074fe4ebf0919baad3519d55073c48581193c7
GoodestUsername <ericdongcannn@hotmail.com>
2022-06-03 22:31:49 -0700

    added conditin to join room

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -31,21 +31,23 @@ const GamePage = () => {
 
   // Socket functions
   const onConnect = useCallback(() => {
-      const cookies = new Cookies();
-      socket.emit("joinGame", {roomId: params.roomId, gameKey: null})
-      socket.emit("register", cookies.get(TOKEN_KEY))
+    const cookies = new Cookies();
+    if (params.roomId) socket.emit("joinGame", {roomId: params.roomId, gameKey: null})
+
+    socket.emit("register", cookies.get(TOKEN_KEY))
   }, [params.roomId, socket])
 
   const onSendRoomCode = useCallback((data: {roomId: string}) => {
-      setRoomId(data.roomId);
+    setRoomId(data.roomId);
   }, []);
 
   const onServerMessage = useCallback((data: {msg: string}) => {
-      setserverMessage(data.msg);
+    setserverMessage(data.msg);
   }, []);
 
   const onIssueWarning = useCallback((data: {msg: string}) => {
-      setWarningMessage(data.msg);
+    console.log(data)
+    setWarningMessage(data.msg);
   }, []);
 
   useEffect(() => {
@@ -67,8 +69,10 @@ const GamePage = () => {
 
   return (
     <div className="App" style={{background: "#000000", color: "#d3d3d3"}}>
-        <button onClick={ () => { socket.emit("createGame", uuidv4().slice(8)) } }>Create Invite Link</button>
-        {roomId && <p>{INVITE_LINK_URL + roomId}</p>}
+        <button onClick={ () => { socket.emit("createGame", uuidv4().slice(0, 8)) } }>Create Invite Link</button>
+        {roomId && !params.roomId &&
+          <p>{INVITE_LINK_URL + roomId}</p>
+        }
         <p>{warningMessage}</p>
         <p>{serverMessage}</p>
         <ToastContainer
