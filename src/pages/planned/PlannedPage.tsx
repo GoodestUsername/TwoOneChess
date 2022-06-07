7dad390cc5e515b33d53350183e30c8e521f062a
GoodestUsername <ericdongcannn@hotmail.com>
2022-06-07 01:09:22 -0700

    beginning of planned page

--- src/pages/planned/PlannedPage.tsx
+++ src/pages/planned/PlannedPage.tsx
@@ -1,6 +1,14 @@
+import { Paper, Container, Box } from "@mui/material";
+
 const PlannedPage = () => {
   return (
     <div className="PlannedPage">
+      <Paper>
+        <Container>
+          <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+          </Box>
+        </Container>
+      </Paper>
     </div>
   );
 }
