c3cc98ca1095dc17c7caac21f79d269c07ad20d3
GoodestUsername <ericdongcannn@hotmail.com>
2022-06-08 17:41:01 -0700

    added h6 for titlevariants in document section

--- src/features/components/DocumentSection.tsx
+++ src/features/components/DocumentSection.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 interface DocumentSectionInterface {
     children: React.ReactNode,
     title : string,
-    titleVariant: 'h1'|'h2'|'h3'|'h4'|'h5',
+    titleVariant: 'h1'|'h2'|'h3'|'h4'|'h5'|'h6',
     titleStyle?: React.CSSProperties | undefined,
 }
 
