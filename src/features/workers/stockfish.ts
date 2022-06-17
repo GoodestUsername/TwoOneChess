74817711e4a22fa839bef9b28fc43678a4a0ba85
GoodestUsername <ericdongcannn@hotmail.com>
2022-06-17 16:47:02 -0700

    fixed let to const

--- src/features/workers/stockfish.ts
+++ src/features/workers/stockfish.ts
@@ -16,23 +16,23 @@ class UciEngineWorker {
         this.moves = [];
         this.resolver = null;
 
-        let _self = this;
+        const _self = this;
         
         this.worker.postMessage('uci');
         this.worker.addEventListener('message', function (e) {  
-            let best  = e.data.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbn])?/);
-            let move  = e.data.match(/pv ([a-h][1-8])([a-h][1-8])([qrbn])?/);
-            let cp    = e.data.match(/cp ([0-9]+)?/);
-          if (move) {
-              _self.moves.push({move: {from: move[1], to: move[2], promotion: move[3]}, cp: parseInt(cp[1])})
-          }
-          if (best && _self.resolver) {
-            let bestMove: ShortMove = { from: best[1], to: best[2], promotion: best[3]}
-            _self.resolver({calcMoves:_self.moves, bestMove: bestMove});
-            _self.resolver = null;
-            _self.moves = [];
-          }
-          });
+            const best  = e.data.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbn])?/);
+            const move  = e.data.match(/pv ([a-h][1-8])([a-h][1-8])([qrbn])?/);
+            const cp    = e.data.match(/cp ([0-9]+)?/);
+            if (move) {
+                _self.moves.push({move: {from: move[1], to: move[2], promotion: move[3]}, cp: parseInt(cp[1])})
+            }
+            if (best && _self.resolver) {
+                const bestMove: ShortMove = { from: best[1], to: best[2], promotion: best[3]}
+                _self.resolver({calcMoves:_self.moves, bestMove: bestMove});
+                _self.resolver = null;
+                _self.moves = [];
+            }
+            });
     }
 
     getMoves(history: Move[]) {
