cc049298cb6f92dca3fc36d9aca9f8c968d64c7d
GoodestUsername <ericdongcannn@hotmail.com>
2022-07-08 17:40:45 -0700

    added store provider to index tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,9 +2,9 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-// styles
-import 'bootstrap/dist/css/bootstrap.css';
+// components / pages
 import GamePage from 'pages/game/GamePage';
 import Layout from 'features/layout/Layout';
 import NotFoundPage from 'pages/404/NotFoundPage';
@@ -13,24 +13,32 @@ import FeaturesPage from 'pages/features/FeaturesPage';
 import AboutMePage from 'pages/about/AboutMePage';
 import TutorialPage from 'pages/tutorial/TutorialPage';
 
+// store
+import store from 'util/store';
+
+// styles
+import 'bootstrap/dist/css/bootstrap.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
+  <Provider store ={store}>
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={< Layout/>}>
-            <Route path="*"         element={ <NotFoundPage/> }/>
-            <Route path="/"         element={ <GamePage/> }/>
-            <Route path="/tutorial" element={ <TutorialPage/> }/>
-            <Route path="/features" element={ <FeaturesPage/> }/>
-            <Route path="/planned"  element={ <PlannedPage/> }/>
-            <Route path="/about"    element={ <AboutMePage/> }/>
-            <Route path="/:roomId"  element={ <GamePage/> }/>
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={< Layout/>}>
+          <Route path="*"         element={ <NotFoundPage/> }/>
+          <Route path="/"         element={ <GamePage/> }/>
+          <Route path="/tutorial" element={ <TutorialPage/> }/>
+          <Route path="/features" element={ <FeaturesPage/> }/>
+          <Route path="/planned"  element={ <PlannedPage/> }/>
+          <Route path="/about"    element={ <AboutMePage/> }/>
+          <Route path="/:roomId"  element={ <GamePage/> }/>
+        </Route>
+      </Routes>
     </BrowserRouter>
+  </Provider>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
