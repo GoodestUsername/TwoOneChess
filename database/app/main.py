8fdb5a03c461fbd07e004ccfbb76867d50c21e16
GoodestUsername <ericdongcannn@hotmail.com>
2022-07-21 19:53:28 -0700

    changed to sqlite from mariadb

--- database/app/main.py
+++ database/app/main.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, make_response, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_mysqldb import MySQL
 
 import os
 
-import db
+
 app = Flask(__name__)
 
 app.debug = True
+app.config['SECRET_KEY'] = 'password'
+app.config['SQLALCHEMY'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 CORS(app)
 
